c7e38da2d1b4e9d50f66885e6e336bb2f95e1c5c
Carla Gama <carla.gaama@gmail.com>
2016-11-08 21:18:32 -0200

    implementação da função fs_remove

--- fs.c
+++ fs.c
@@ -39,54 +39,100 @@ typedef struct {
 
 dir_entry dir[128];
 
-
+/*Inicia o sistema de arquivos e suas estruturas internas. Esta
+função é automaticamente chamada pelo interpretador de comandos no
+início do sistema. Esta função deve carregar dados do disco para restaurar
+um sistema já em uso e é um bom momento para verificar se o disco
+está formatado.*/
 int fs_init() {
   printf("Função não implementada: fs_init\n");
   return 1;
 }
-
+/*Inicia o dispositivo de disco para uso, iniciando e escre-
+vendo as estruturas de dados necessárias.*/
 int fs_format() {
   printf("Função não implementada: fs_format\n");
   return 0;
 }
-
+/*Retorna o espaço livre no dispositivo em bytes.*/
 int fs_free() {
   printf("Função não implementada: fs_free\n");
   return 0;
 }
-
+/*Lista os arquivos do diretório, colo-
+cando a saída formatada em buffer . O formato é simples, um arquivo
+por linha, seguido de seu tamanho e separado por dois tabs. Observe
+que a sua função não deve escrever na tela.*/
 int fs_list(char *buffer, int size) {
   printf("Função não implementada: fs_list\n");
   return 0;
 }
-
+/*Cria um novo arquivo com nome
+file_name e tamanho 0. Um erro deve ser gerado se o arquivo já existe.*/
 int fs_create(char* file_name) {
   printf("Função não implementada: fs_create\n");
   return 0;
 }
-
+/*Remove o arquivo com nome
+file_name . Um erro deve ser gerado se o arquivo não existe.*/
 int fs_remove(char *file_name) {
-  printf("Função não implementada: fs_remove\n");
-  return 0;
+  int i;
+  /*busco o nome do arquivo e salvo o primeiro bloco*/
+  for(i = 0; i < 128; i++){
+    if(strcmp(dir_entry[i].name, file_name) == 0){
+      dir_entry[i].used = 0;
+      unsigned short posicaoInicialFAT = dir_entry[i].first_block;
+      break;
+    }
+  }
+  if(strcmp(dir_entry[i].name, file_name) != 0){
+    perror("Arquivo nao existe!\n");
+    return 1;
+  }else{
+    /*limpo no fat os ponteiros para o arquivo, marcando como 1 == livre*/
+    int prox = fat[posicaoInicialFAT];
+    while(prox != 2){
+      prox = fat[prox];
+      fat[prox] = 1;
+    }
+    fat[prox] = 1;
+    return 0;
+  }
 }
-
+/*Abre o arquivo file_name
+para leitura (se mode for FS_R ) ou escrita (se mode for FS_W ). Ao abrir um
+arquivo para leitura, um erro deve ser gerado se o arquivo não existe.
+Ao abrir um arquivo para escrita, o arquivo deve ser criado ou um ar-
+quivo pré-existente deve ser apagado e criado novamente com tamanho
+0. Retorna o identificador do arquivo aberto, um inteiro, ou -1 em caso
+de erro.*/
 int fs_open(char *file_name, int mode) {
   printf("Função não implementada: fs_open\n");
   return -1;
 }
-
+/*Fecha o arquivo dado pelo identificador de arquivo
+file . Um erro deve ser gerado se não existe arquivo aberto com este
+identificador.*/
 int fs_close(int file)  {
   printf("Função não implementada: fs_close\n");
   return 0;
 }
-
+/*Escreve size bytes do
+buffer no arquivo aberto com identificador file . Retorna quantidade
+de bytes escritos (0 se não escreveu nada, -1 em caso de erro). Um erro
+deve ser gerado se não existe arquivo aberto com este identificador ou
+caso o arquivo tenha sido aberto para leitura.*/
 int fs_write(char *buffer, int size, int file) {
   printf("Função não implementada: fs_write\n");
   return -1;
 }
-
+/*Lê no máximo size by-
+tes no buffer do arquivo aberto com identificador file . Retorna quan-
+tidade de bytes efetivamente lidos (0 se não leu nada, o que indica que o
+arquivo terminou, -1 em caso de erro). Um erro deve ser gerado se não
+existe arquivo aberto com este identificador ou caso o arquivo tenha sido
+aberto para escrita.*/
 int fs_read(char *buffer, int size, int file) {
   printf("Função não implementada: fs_read\n");
   return -1;
 }
-
